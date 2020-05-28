93c955426a171fb1d89f98700f6b93cadd5223ee
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-28 20:54:29 +0430

    Added function body from sippet and modified it

--- wifiPass.py
+++ wifiPass.py
@@ -4,7 +4,33 @@ import tkinter as tk
 
 
 def getPasswords():
-    pass
+    results = ''
+    # Getting saved wifi profiles using cmd command netsh wlan show profiles
+    wifiNames = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+    wifiNames = wifiNames.decode('utf-8').split('\n')
+    wifiNames = [i.split(':')[1][1:-1]
+                 for i in wifiNames if 'All User Profile' in i]
+
+    # Looping  in names and using cmd command netsh wlan show profile WIFI_NAME with KEY=CLEAR switch and print result
+    for name in wifiNames:
+        tempRes = ''
+        password = subprocess.check_output(
+            ['netsh', 'wlan', 'show', 'profile', name, 'KEY=CLEAR']).decode('utf-8').split('\n')
+        password = [line.split(':')[1][1:-1]
+                    for line in password if 'Key Content' in line]
+        try:
+            print('{:<30}| {:<}'.format(name, password[0]))
+            tempRes += '{:<30}| {:<}\n'.format(name, password[0])
+        except IndexError:
+            print('{:<30}| {:<}'.format(
+                name, 'No PASSWORD - other authentication protocol might been used'))
+            tempRes += '{:<30}| {:<}\n'.format(
+                name, 'No PASSWORD - other authentication protocol might been used')
+
+        results += tempRes
+
+    data.delete(1.0, tk.END)
+    data.insert(tk.END, results)
 
 
 # ------------- GUI -------------------
